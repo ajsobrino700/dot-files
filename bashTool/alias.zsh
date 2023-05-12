9be2e3a0d00c3a1ee2617af124aed688aa06b159
ajsobrino <ajsobrino@MacBook-Pro-de-Antonio.local>
2023-05-12 12:37:06 +0200

    Add comment on terminal

--- bashTool/alias.zsh
+++ bashTool/alias.zsh
@@ -2,7 +2,11 @@
 
 alias nv="nvim"
 
+# It is mandatory avaible the font allows icons ( I install font-hack)
+# brew tap homebrew/cask-fonts
+# brew install --cask font-hack-nerd-font
 alias ls="lsd"
 alias ll="lsd -l"
 
 alias cat="bat"
+
