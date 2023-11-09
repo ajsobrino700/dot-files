4e05dd5ff2610ade2f90d98c63e67a93cd1deac3
ajsobrino <ajsobrino700@gmail.com>
2023-11-09 19:46:38 +0100

    little change in prompt

--- zsh/terminal/prompt.zsh
+++ zsh/terminal/prompt.zsh
@@ -17,6 +17,6 @@ local second_line_well="%F{40}%# %F{77}>%F{156}>%F{230}> "
 local second_line="%(?.${second_line_well}.${second_line_error})"
 
 
-PROMPT="%F{215}%m %F{208} %~ $prompt_newline ${second_line}%F{reset_color}"
+PROMPT="%F{215}%m %F{208} %~$prompt_newline${second_line}%F{reset_color}"
 
 RPROMPT='%(?.✅.❌) %D{%H:%M:%S}'
