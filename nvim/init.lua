1b689f47302d7bfa5e64a1788218c90346697bd7
ajsobrino <antonio.sobrino@feedzai.com>
2024-04-26 20:12:58 +0200

    Remove the seetings for nvim

--- nvim/init.lua
+++
@@ -1,3 +0,0 @@
-require("settings/remap")
-require("settings/lazy")
-require("settings/basic_settings")
--- nvim/plugins/feline.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"feline-nvim/feline.nvim",
-	branch = "0.5-compat",
-	config = function()
-		require('feline').setup()
-	end
-}
