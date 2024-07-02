7e6292a1da66273aba5794f6f1ba48c1dc51b047
ajsobrino <antonio.sobrino@feedzai.com>
2024-07-02 12:09:39 +0200

    Add some comment

--- nvim/lua/settings/plugins/none-ls.lua
+++ nvim/lua/settings/plugins/none-ls.lua
@@ -5,9 +5,10 @@ local M = {}
 function M.setup()
 	null_ls.setup({
 		sources = {
+			-- lua
 			null_ls.builtins.formatting.stylua,
+			-- JS and TS
 			null_ls.builtins.formatting.prettier,
-
 			-- python
 			null_ls.builtins.formatting.black,
 			-- markdown
